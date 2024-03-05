b96496a1cc1d918e4f56d55166d817536fd286fd
Maggie Cook <mkcook4@asu.edu>
2024-03-05 11:35:24 -0800

    unparsable IDs to csv

--- src/frenda_brenda/refseq2ID.sh
+++ src/frenda_brenda/refseq2ID.sh
@@ -14,10 +14,12 @@ IFS=$'\n' read -d '' -r -a values2 <<< "$column_data2"
 
 output_file1="Files/Reaction.csv"
 output_file2="Files/SpeciesBaseMechanisms.csv"
+inaccessible_file="Files/Inaccessible_IDs.csv"
 
 # Add header line to the output files
 echo "Accession Number,EC,Species,Label,Enzyme,Reaction ID,Mechanism,Substrates,Products,Km,Kcat,Inhibitors,KI" > "$output_file1"
 echo "Label,EC,Type,StartingConc,Conc,Mechanisms,Parameters" > "$output_file2"
+echo "Accession Number,Conc,EC" > "$inaccessible_file"
 
 for index in "${!values1[@]}"; do
     value1="${values1[index]}"
@@ -27,8 +29,13 @@ for index in "${!values1[@]}"; do
     conversion=$(efetch -db protein -id "$value1" -format gpc -mode xml | xtract -insd Protein EC_number)
     ec_number=$(echo "$conversion" | awk -F'\t' '{print $2}')
 
-    # Corrected the formatting for "SpeciesBaseMechanisms.csv" to ensure all fields are on the same line
-    echo "$value1,$ec_number,Enzyme,$value2" >> "$output_file2"
-
-    awk -F'\t' -v OFS=',' '{ print $1, $2 }' <<< "$conversion" >> "$output_file1"
+    if [ -z "$ec_number" ] || [ "$ec_number" = "-" ]; then
+        # If EC_number is empty or '-', print to inaccessible file
+        echo "$value1,$value2" >> "$inaccessible_file"
+    else
+        # Print to SpeciesBaseMechanisms.csv
+        echo "$value1,$ec_number,Enzyme,$value2" >> "$output_file2"
+        # Print to Reaction.csv
+        awk -F'\t' -v OFS=',' '{ print $1, $2 }' <<< "$conversion" >> "$output_file1"
+    fi
 done
